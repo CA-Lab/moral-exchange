d84229bebb2ba55d98c4cc973672823c16e76a5a
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-05-21 13:23:40 -0500

    Better formating, patched a couple of missing ]'s
    Also: converted sets to tuples as they're a more appropriate data structure.

--- Data/boats.py
+++ Data/boats.py
@@ -1,3 +1,39 @@
 #coding: utf-8
-#The structure of data is a dictionary of dictionary with sets as values {'boats': set(['clan','great houses'])}. Source: Barnes, Robert H. (1998) Alliance, Exchange, and the Organization of Boat Corporations in Lamalera (E. Indonesia). In: Thomas Schweizer and Douglas R. White, Kinship, Networks, and Exchange. Cambridge University Press, p. 100.
-boats={'Muko_Tena': set(['Ata_Kei', 'Ata_Kei']),'Demo_Sapa': set(['Beliko_Lolo','Lali_Nama_Papa']),'Naka_Tena': set(['Tufa_Ona','Tufa_Ona']),'Beraso_Sapa': set(['Lela_Ona','Sinu_Lango']),'Bela_Sapa': set(['Lela_Ona','Sinu_Lango']),'Nota_Tena': set(['Lama_Nudek','Lama_Nudek']),'Menula_Belolong': set(['Lama_Keera','Badi_Langu']),'Pato_Tena': set(['Lama_Nudek','Lama_Nudek']),'Kebela_Tena': set(['Lefo_Tuka','Dasi_Langu']),'Boli_Sapa': set(['Hari_Ona','Hari_Ona'),'Geleko_Tena': set(['Tapo_Ona','Guna_Langu']),'Boko_Lolo': set(['Beliko_Lolo','Lali_Nama_Papa']),'Sika_Tena': set(['Sula_Ona','Kelore_Langu']),'Bui_Puka': set(['Beliko_Lolo','Teti_Nama_Papa']),'Sia_Apu': set(['Lama_Nifa','Lama_Nifa'),'Nara_Tena': set(['Lama_Kera','Perafi_Langu']),'Kopo_Paker': set(['Ole_Ona','Ole_Ona']),'Teti_Heri': set(['Bata_Fo','Kaja_Langu']),'Holo_Sapa': set(['Bata_Ona','Kifa_Langu']),'Soge_Tena': set(['Tapo_Ona','Musi_Langu']),'Horo_Tena': set(['Bata_Ona','Kifa_Langu']),'Dolu_Tena': set(['Sula_Ona','Kebesa_Langu']),'Sili_Tena': set(['Bata_Ona','Kifa_Langu']),'Kena_Puka': set(['Bedi_Ona','Miku_Lnagu']),'Kelulus': set(['Bedi_Ona','Muri_Langu']),'Kebako_Puka': set(['Beta_Ona','Ola_Langu']),'Tena_Ana': set(['Bata_Ona','Kelake_Langu']),'Sinu_Sapa': set(['Bata_Ona','Kifa_Langu']),'Jafa_Tena': set(['Bata_Ona','Jafa_Langu'])}
+
+# The data structure is a dictionary, boat names are keys, values are
+# tuples of clans and great houses
+# {'boat': ('clan','great house')}
+
+# Source: Barnes, Robert H. (1998) Alliance, Exchange, and the
+# Organization of Boat Corporations in Lamalera (E. Indonesia). In:
+# Thomas Schweizer and Douglas R. White, Kinship, Networks, and
+# Exchange. Cambridge University Press, p. 100.
+boats={'Muko_Tena': ('Ata_Kei', 'Ata_Kei'),
+       'Demo_Sapa': ('Beliko_Lolo','Lali_Nama_Papa'),
+       'Naka_Tena': ('Tufa_Ona','Tufa_Ona'),
+       'Beraso_Sapa': ('Lela_Ona','Sinu_Lango'),
+       'Bela_Sapa': ('Lela_Ona','Sinu_Lango'),
+       'Nota_Tena': ('Lama_Nudek','Lama_Nudek'),
+       'Menula_Belolong': ('Lama_Keera','Badi_Langu'),
+       'Pato_Tena': ('Lama_Nudek','Lama_Nudek'),
+       'Kebela_Tena': ('Lefo_Tuka','Dasi_Langu'),
+       'Boli_Sapa': ('Hari_Ona','Hari_Ona'),
+       'Geleko_Tena': ('Tapo_Ona','Guna_Langu'),
+       'Boko_Lolo': ('Beliko_Lolo','Lali_Nama_Papa'),
+       'Sika_Tena': ('Sula_Ona','Kelore_Langu'),
+       'Bui_Puka': ('Beliko_Lolo','Teti_Nama_Papa'),
+       'Sia_Apu': ('Lama_Nifa','Lama_Nifa'),
+       'Nara_Tena': ('Lama_Kera','Perafi_Langu'),
+       'Kopo_Paker': ('Ole_Ona','Ole_Ona'),
+       'Teti_Heri': ('Bata_Fo','Kaja_Langu'),
+       'Holo_Sapa': ('Bata_Ona','Kifa_Langu'),
+       'Soge_Tena': ('Tapo_Ona','Musi_Langu'),
+       'Horo_Tena': ('Bata_Ona','Kifa_Langu'),
+       'Dolu_Tena': ('Sula_Ona','Kebesa_Langu'),
+       'Sili_Tena': ('Bata_Ona','Kifa_Langu'),
+       'Kena_Puka': ('Bedi_Ona','Miku_Lnagu'),
+       'Kelulus': ('Bedi_Ona','Muri_Langu'),
+       'Kebako_Puka': ('Beta_Ona','Ola_Langu'),
+       'Tena_Ana': ('Bata_Ona','Kelake_Langu'),
+       'Sinu_Sapa': ('Bata_Ona','Kifa_Langu'),
+       'Jafa_Tena': ('Bata_Ona','Jafa_Langu')}
