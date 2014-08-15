4bc413355925424d11544688fdb926d468f4823a
jmsiqueiros <jmsiqueiros@member.fsf.org>
2014-08-15 17:28:44 -0500

    repaired fitness and trust functions

--- simulations/gt_trust.py
+++ simulations/gt_trust.py
@@ -7,11 +7,10 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-
-
-c = 0
-d = 1
-
+nt = nx.Graph()
+nt.add_node( 'a' )
+nt.add_node( 'b' )
+nt.add_edge( 'a', 'b', t=10.0 )
 
 def init(): #posteriormente arreglar para escoger la condicion de
     #incio: dos cooperadores, cooperador vs detractor, dos
@@ -22,18 +21,14 @@ def init(): #posteriormente arreglar para escoger la condicion de
 
     time = 0
     
-    nt = nx.Graph()
-    nt.add_node( 'a' )
-    nt.add_node( 'b' )
-
     #nt.node['a']['id'] = c
     #nt.node['b']['id'] = d
 
-    nt.node['a']['w'] = 10 #fitness inicial
-    nt.node['b']['w'] = 10 #fitness inicial
+    nt.node['a']['w'] = 10.0 #fitness inicial
+    nt.node['b']['w'] = 10.0 #fitness inicial
 
-    nt.add_edge( 'a', 'b' )
-    nt.edge['a']['b']['t'] = 10 #Confianza incial de la relacion
+
+
     
     nodes = nt.nodes(True)
      
@@ -54,65 +49,63 @@ positions = nx.random_layout(nt)
 #     pl.title('t = ' + str(time))
 #     plt.show() 
 
-def node_strategy(nodes): #players get their strategies randomly
-
+def node_strategy(): #players get their strategies randomly    
     x = 0.5 #eventualmente modificar para que quien corra la simulacion elija la probabilidad de que el nodo salga cooperador o detractor.
     for i in nt.nodes():
         if x > rd.random():
-            nt.node[i]['id'] = c
+            nt.node[i]['id'] = 'c'
         else:
-            nt.node[i]['id'] = d
-    return nt.nodes(True)
+            nt.node[i]['id'] = 'd'
 
 
-def fitness(nodes): #funcion de evaluacion y actualizacion del fitness
+
+def fitness(): #funcion de evaluacion y actualizacion del fitness
     #de cada competidor
-    for i, j in nodes:
-        for i, j in nt.edges():
-            if nt.node[i]['id'] == c and nt.node[j]['id'] == d:
-                nt.node[i]['w'] = nt.node[i]['w'] - 1
-                nt.node[j]['w'] = nt.node[j]['w'] - 2
+    for i, j in nt.edges():
+        if nt.node[i]['id'] == 'c' and nt.node[j]['id'] == 'd':
+            nt.node[i]['w'] -= 1.0
+            nt.node[j]['w'] += 2.0
             
-            if nt.node[i]['id'] == d and nt.node[j]['id'] == c:
-                nt.node[i]['w'] = nt.node[i]['w'] + 2
-                nt.node[j]['w'] = nt.node[j]['w'] - 1
+        if nt.node[i]['id'] == 'd' and nt.node[j]['id'] == 'c':
+            nt.node[i]['w'] += 2.0
+            nt.node[j]['w'] -= 1.0
 
-            if nt.node[i]['id'] == c and nt.node[j]['id'] == c:
-                nt.node[i]['w'] = nt.node[i]['w'] + .5
-                nt.node[j]['w'] = nt.node[j]['w'] + .5
+        if nt.node[i]['id'] == 'c' and nt.node[j]['id'] == 'c':
+            nt.node[i]['w'] += 0.5
+            nt.node[j]['w'] += 0.5
 
-            if nt.node[i]['id'] == d and nt.node[j]['id'] == d:
-                nt.node[i]['w'] = nt.node[i]['w'] - 2
-                nt.node[j]['w'] = nt.node[j]['w'] - 2
-    return nodes
+        if nt.node[i]['id'] == 'd' and nt.node[j]['id'] == 'd':
+            nt.node[i]['w'] -= 2
+            nt.node[j]['w'] -= 2
 
 
-def trust(nodes): #Funcion de evaluacion de la confianza
-    for i, j in nodes:
-        for i, j in nt.edges():
-            if nt.node[i]['id'] != nt.node[j]['id']:
-                nt.edge[i][j]['t'] = nt.edge[i][j]['t'] - 1
+
+def trust(): #Funcion de evaluacion de la confianza
+    for i, j in nt.edges():
+        if nt.node[i]['id'] != nt.node[j]['id']:
+            nt.edge[i][j]['t'] -= 1
                 
-            if nt.node[i]['id'] == c and nt.node[j]['id'] == c:
-                nt.edge[i][j]['t'] = nt.edge[i][j]['t'] + 2
+        if nt.node[i]['id'] == 'c' and nt.node[j]['id'] == 'c':
+            nt.edge[i][j]['t'] += 2
 
-            if nt.node[i]['id']==d and nt.node[j]['id']==d:
-                nt.edge[i][j]['t'] = nt.edge[i][j]['t'] - 2
+        if nt.node[i]['id']=='d' and nt.node[j]['id']=='d':
+            nt.edge[i][j]['t'] -= 2
 
-            trust_1 = nt.edge[i][j]['t']
-        return trust_1
+import pprint
 
 def step():
     global time, nt, positions
     
     time += 1
-    
-    for nodes in nt.nodes():
-        nodes = node_strategy(nodes)
-        fitness(nodes)
-        t_degree = trust(nodes)
-    
-    return  nodes, t_degree
+    pprint.pprint( nt.nodes(True) )
+    node_strategy()
+    fitness()
+    pprint.pprint( nt.nodes(True) )
+    trust()
+    pprint.pprint(nt.get_edge_data('a','b'))
+
+
+
 
 step()
 #draw()
