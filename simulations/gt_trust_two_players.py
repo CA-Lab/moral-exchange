cb45797e09e5df43f2520da4ed0e13c3ce816b6c
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2014-08-22 17:38:36 -0500

    implement two strategies

--- simulations/gt_trust_two_players.py
+++ simulations/gt_trust_two_players.py
@@ -2,10 +2,32 @@ import random
 import matplotlib
 import pylab as pl
 import matplotlib.pyplot as plt
+import pprint
 
-
-def random_strategy(): #players get their strategies randomly    
+def random_strategy(player): #players get their strategies randomly    
     return random.choice(['d','c'])
+
+def tit_for_tat(player):
+    if player == 'a':
+        other_player = 'b'
+    elif player == 'b':
+        other_player = 'a'
+    
+    return T[t]['s_'+other_player]
+
+
+def proportional_tit_for_tat(player):
+    if player == 'a':
+        other_player = 'b'
+    elif player == 'b':
+        other_player = 'a'
+
+    # all choices of the other player
+    choices = []
+    for s in T:
+        choices += s['s_'+other_player]
+
+    return random.choice( choices )
 
 
 def step(state, strategy=random_strategy): #funcion de evaluacion y actualizacion del fitness
@@ -13,54 +35,53 @@ def step(state, strategy=random_strategy): #funcion de evaluacion y actualizacio
 
     if state['s_a'] == 'c' and state['s_b'] == 'd':
         return {'f_a': state['f_a']-2,
-                's_a': strategy(),
+                's_a': strategy('a'),
                 'f_b': state['f_b']+2,
-                's_b': strategy(),
+                's_b': strategy('b'),
                 'trust': state['trust']-1}
 
             
     if state['s_a'] == 'd' and state['s_b'] == 'c':
         return {'f_a': state['f_a']+2,
-                's_a': strategy(),
+                's_a': strategy('a'),
                 'f_b': state['f_b']-2,
-                's_b': strategy(),
+                's_b': strategy('b'),
                 'trust': state['trust']-1}
 
 
     if state['s_a'] == 'c' and state['s_b'] == 'c':
         return {'f_a': state['f_a']+1,
-                's_a': strategy(),
+                's_a': strategy('a'),
                 'f_b': state['f_b']+1,
-                's_b': strategy(),
+                's_b': strategy('b'),
                 'trust': state['trust']+2}
 
     if state['s_a'] == 'd' and state['s_b'] == 'd':
         return {'f_a': state['f_a']-1,
-                's_a': strategy(),
+                's_a': strategy('a'),
                 'f_b': state['f_b']-1,
-                's_b': strategy(),
+                's_b': strategy('b'),
                 'trust': state['trust']-2}
 
 
 
 initial_state = {'f_a': 10,
-                 's_a': 'c',
+                 's_a': random_strategy('a'),
                  'f_b': 10,
-                 's_b': 'c',
+                 's_b': random_strategy('b'),
                  'trust': 10,}
 
 T = [ initial_state, ]
 
-n=0
-while T[n]['trust']>0:
-    T.append(step(T[n]))
-    n +=1
+t=0
+while T[t]['trust']>0 and t<100:
+    T.append(step(T[t], strategy=proportional_tit_for_tat) )
+    t +=1
 
 
 
 
-
-
+# plot
 trust_list = []
 fitness_a = []
 fitness_b = []
