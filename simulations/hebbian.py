5bca8d4046b4fe8b5a2b49b59cafc5a521965ff4
jmsiqueiros <jmsiqueiros@member.fsf.org>
2015-02-05 14:07:16 -0600

    Hopfield/Hebbian rule-based network with perturbation refrenced to Watson et al, 2011..

--- simulations/hebbian.py
+++ simulations/hebbian.py
@@ -11,22 +11,24 @@ import pprint as ppt
 
 time_list = []
 energy_state = []
+perturbation_period = 500
+pert_accu = 0
 
 def init_full():
-    global time, g, positions, E
+    global time, g, positions, U
     E = 0
     time = 0
-    g = nx.complete_graph(100)
+    g = nx.complete_graph(50)
 
     for i in g.nodes():
         g.node[i]['s'] = rd.choice([1,-1])
     
     for i,j in g.edges():
-        g.edge[i][j]['w'] = rd.choice([-2,-1,0,1,2])    
+        g.edge[i][j]['w'] = rd.choice([-1,1])    
 
     
 def init_watts():
-    global time, g, positions, E
+    global time, g, positions, U
     E = 0
     time = 0
 
@@ -37,20 +39,20 @@ def init_watts():
         g.node[i]['s'] = rd.choice([1,-1])
     
     for i,j in g.edges():
-        g.edge[i][j]['w'] = rd.choice([-2,-1,0,1,2])
+        g.edge[i][j]['w'] = rd.choice([-1,1])
 
 
 def init_erdos():
-    global time, g, positions, E
+    global time, g, positions, U
     E = 0
     time = 0
-    g = nx.erdos_renyi_graph(25, .3)
+    g = nx.erdos_renyi_graph(50, .1)
 
     for i in g.nodes():
         g.node[i]['s'] = rd.choice([1,-1])
     
     for i,j in g.edges():
-        g.edge[i][j]['w'] = rd.choice([-2,-1,0,1,2])
+        g.edge[i][j]['w'] = rd.choice([-1,1])
 
 
 def draw():
@@ -58,70 +60,94 @@ def draw():
     nx.draw(g, pos = positions,
             node_color = [g.node[i]['s'] for i in g.nodes_iter()],
             with_labels = True, edge_color = 'c',
+            width = [g.edge[i][j]['w'] for (i,j) in g.edges_iter()],
             cmap = pl.cm.autumn, vmin = 0, vmax = 1)
     
     pl.axis('image')
     pl.title('t = ' + str(time))
-    #pl.title('Energy = ' + str(E))
     plt.show() 
 
 def local_u(i):
     m = []
+    i = rd.choice(g.nodes())
+
     for j in g.neighbors(i):
-        if g.node[i]['s'] == g.node[j]['s']:
-            m.append( 1 )
+        m.append( g.edge[i][j]['w'] * g.node[j]['s'] )
     return sum(m)        
 
 def global_u():
+   
     U = []
-    for i, j in g.edges():
-        if g.node[i]['s'] == 1 and g.node[j]['s'] == 1:
-            U.append( g.edge[i][j]['w']  )
-    return sum( U )
+    for i in g.nodes():
+        U.append( local_u( i ) )
+        #print sum( U )
+    return sum(U)
 
-    # U = []
-    # for i in g.nodes():
-    #     U.append( local_u( i ) )
-    # return sum(U)
+def randomize_states():
+    for i in g.nodes():
+        g.node[i]['s'] = rd.choice([1,-1])
 
-def step():
-    global time, g, positions, E
+def hebbian():
+    global time, g, positions, U
 
-    time += 1
-    states = []
-    m = []
-    """ef for energy function"""
-    ef = []
-
-    i = rd.choice(g.nodes())
-    for j in g.neighbors(i):
-        m.append( g.edge[i][j]['w'] * g.node[j]['s'] )
-        
-    e = sum(m)
     
-    if e >= 0:
+    i = rd.choice(g.nodes())
+
+    
+    if local_u( i ) >= 0:
         g.node[i]['s'] = 1
     else:
         g.node[i]['s'] = -1
-                
+
+    for j in g.neighbors( i ):
+        if g.node[i]['s'] == g.node[j]['s']:
+            g.edge[i][j]['w'] += 1
+        else:
+            g.edge[i][j]['w'] -= 1
+
+def hopfield():
+
+    i = rd.choice(g.nodes())
+    
+    if local_u(i) >= 0:
+        g.node[i]['s'] = 1
+    else:
+        g.node[i]['s'] = -1
+            
+def step():
+    global time, g, positions, U, pert_accu, perturbation_period
+    time += 1
+    if pert_accu == perturbation_period:
+        pert_accu = 0
+        randomize_states()
+    else:
+        pert_accu += 1
+        
+    # if time <= 25:
+    #     hopfield()
+    # else:
+    hebbian()
     
 
-        
     time_list.append(time)
-    energy_state.append(global_u())
+    energy_state.append( global_u() )
 
+def no_draw():
+    global time
+    print time
 
         
 import pycxsimulator
 #init()
-init_full()
+#init_full()
 #init_watts()
-#init_erdos()
+init_erdos()
 #init_barabasi()
 positions = nx.spring_layout(g)
-pycxsimulator.GUI().start(func = [init_full, draw, step])
+pycxsimulator.GUI().start(func = [init_erdos, no_draw, step])
+#pycxsimulator.GUI().start(func = [init_erdos, draw, step])
 plt.cla()
-plt.plot(time_list, energy_state, 'bs-')
+plt.plot(time_list, energy_state, 'b-')
 plt.xlabel('Time')
 plt.ylabel('Energy states')
 plt.savefig('hebb_plot.png')
