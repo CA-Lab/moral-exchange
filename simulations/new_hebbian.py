8bb8f52a54a0050dfd3a0eb1d50959642ec5d192
jmsiqueiros <jmsiqueiros@member.fsf.org>
2015-02-12 19:40:39 -0600

    Added code for sync steps and scatter plot visualization.

--- simulations/new_hebbian.py
+++ simulations/new_hebbian.py
@@ -14,7 +14,11 @@ time_list = []
 energy_state_g = []
 energy_state_o = []
 
-perturbation_period = 1000
+T_list = [0, ]
+T = 0
+U_plot = [0, ]
+
+perturbation_period = 1001
 pert_accu = 0
 time = 0
 m = []
@@ -28,29 +32,29 @@ def init_full():
         g.node[i]['s'] = rd.choice([1,-1])
     
     for i,j in g.edges():
-        g.edge[i][j]['w'] = 1
+        g.edge[i][j]['w'] = rd.choice([-1,0,1])
 
 
     #should weights be initialized to 0, 1 or 0.01?
     o = g.copy()
-    for i,j in o.edges():
-        o.edge[i][j]['w'] = 0
+    # for i,j in o.edges():
+    #     o.edge[i][j]['w'] = 0
     
 def init_erdos():
     global time, g, o, positions, U
     
     time = 0
-    g = nx.erdos_renyi_graph(560, .08)
+    g = nx.erdos_renyi_graph(120, .06)
 
     for i in g.nodes():
         g.node[i]['s'] = rd.choice([1,-1])
     
     for i,j in g.edges():
-        g.edge[i][j]['w'] = rd.choice([-1,1])
+        g.edge[i][j]['w'] = rd.choice([-1,0,1])
 
     o = g.copy()
-    for i,j in o.edges():
-        o.edge[i][j]['w'] = 0
+    # for i,j in o.edges():
+    #     o.edge[i][j]['w'] = 0
     
 def init_watts():
     global time, g, o, positions, U
@@ -136,7 +140,9 @@ def local_uo(i, o):
 
 
 def global_uo(o):
+    global pert_accu, perturbation_period
     U = 0
+    U_plot = []
     for i in o.nodes():
         U += local_uo( i, g )
     return U
@@ -148,7 +154,7 @@ def global_uo(o):
 
 
 def step():
-    global time, g, o, positions, pert_accu, perturbation_period
+    global time, g, o, positions, pert_accu, perturbation_period, T
     time += 1
 
     if pert_accu == perturbation_period:
@@ -185,57 +191,68 @@ def step():
     time_list.append(time)
     energy_state_o.append( global_uo(o) )
     #energy_state_g.append( global_ul(g) )
+    
+    if time%1000 == 0:
+        T += 1
+        T_list.append( T )
+        U_plot.append( global_uo(o) )
 
 
 
-# def step_sync():
-#     global time, g, o, positions, pert_accu, perturbation_period
-#     time += 1
 
-#     if pert_accu == perturbation_period:
-#         pert_accu = 0
-#         randomize_states( g )
-#     else:
-#         pert_accu += 1
+def step_sync():
+    global time, g, o, positions, pert_accu, perturbation_period, T
+    time += 1
+
+    if pert_accu == perturbation_period:
+        pert_accu = 0
+        randomize_states( g )
+    else:
+        pert_accu += 1
 
         
-#     i = rd.choice(g.nodes())
+    i = rd.choice(g.nodes())
 
-#     r = 0.005
+    r = 0.005
     
-#     m_1 = 0
-#     for j in g.neighbors(i) and o.neighbors(i):
-#         m_1 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] + r) * g.node[i]['s'] * g.node[j]['s']
+    m_1 = 0
+    for j in g.neighbors(i) and o.neighbors(i):
+        m_1 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] + r) * g.node[i]['s'] * g.node[j]['s']
 
 
-#     m_2 = 0
-#     for j in g.neighbors(i) and o.neighbors(i):
-#         m_2 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] -r) * g.node[i]['s']  * g.node[j]['s']
+    m_2 = 0
+    for j in g.neighbors(i) and o.neighbors(i):
+        m_2 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] -r) * g.node[i]['s']  * g.node[j]['s']
 
         
-#     if m_1 != m_2:
-#         if m_1 > m_2:
-#             g.edge[i][j]['w'] += r
-#         else:
-#             g.edge[i][j]['w'] -= r
+    if m_1 != m_2:
+        if m_1 > m_2:
+            g.edge[i][j]['w'] += r
+        else:
+            g.edge[i][j]['w'] -= r
 
     
-#     #node_state( i, g )
+    #node_state( i, g )
     
 
-#     #m = []
-#     h = g.copy()
-#     p = o.copy()
+    #m = []
+    h = g.copy()
+    p = o.copy()
     
-#     for i in g.nodes() and o.nodes():
-#         node_state(i,h)
+    for i in g.nodes() and o.nodes():
+        node_state(i,h)
 
-#     g = h.copy()
-#     o = p.copy()
+    g = h.copy()
+    o = p.copy()
 
-#     time_list.append(time)
-#     energy_state_o.append( global_uo(o) )
+    time_list.append(time)
+ #   energy_state_o.append( global_uo(o) )
 #     #energy_state_g.append( global_ul(g) )
+    
+    if time%1000 == 0:
+        T += 1
+        T_list.append( T )
+        U_plot.append( global_uo(o) )
 
 
 def no_draw():
@@ -245,16 +262,17 @@ def no_draw():
         
 import pycxsimulator
 #init()
-init_full()
+#init_full()
 #init_watts()
-#init_erdos()
+init_erdos()
 #init_barabasi()
 positions = nx.spring_layout(g)
-pycxsimulator.GUI().start(func = [init_full, no_draw, step])
+pycxsimulator.GUI().start(func = [init_erdos, no_draw, step_sync])
 #pycxsimulator.GUI().start(func = [init_full, draw, step])
 plt.cla()
 #plt.plot(time_list, energy_state_g, 'b+')
-plt.plot(time_list, energy_state_o, 'r-')
+#plt.plot(time_list, energy_state_o, 'r-')
+plt.scatter( T_list, U_plot, c=u'r', marker=u'D' )
 plt.xlabel('Time')
 plt.ylabel('Global Utility')
 plt.savefig('new_hebbian_plot.png')
