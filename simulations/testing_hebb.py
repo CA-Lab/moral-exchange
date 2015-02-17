e746057064f56ddc026bc060d9cc7086dfc017ce
jmsiqueiros <jmsiqueiros@member.fsf.org>
2015-02-17 12:11:10 -0600

    Testing version of hebbian learning model2

--- simulations/testing_hebb.py
+++ simulations/testing_hebb.py
@@ -25,19 +25,19 @@ U_plot = [0, ]
 def init_full():
     global time, g, o, positions
 
-    g = nx.complete_graph(100) 
+    g = nx.complete_graph(50) 
 
     for i in g.nodes():
         g.node[i]['s'] = rd.choice([1,-1])
     
     for i,j in g.edges():
-        g.edge[i][j]['w'] = rd.choice([1,0,-1])
+        g.edge[i][j]['w'] = 0
 
 
     # should weights be initialized to 0, 1 or 0.01?
     o = g.copy()
     for i,j in o.edges():
-        o.edge[i][j]['w'] = 0
+        o.edge[i][j]['w'] = rd.choice([1,0,-1])
 
 def init_erdos():
     global time, g, o, positions, U
@@ -68,11 +68,9 @@ def draw():
     plt.show() 
 
 def randomize_states( g ):
-    for i in g.nodes():
+    for i in o.nodes():
         state = rd.choice([1,-1])
-        g.node[i]['s'] = state
         o.node[i]['s'] = state
-
 
 def local_uo(i, o):
     u_o = 0
@@ -81,90 +79,94 @@ def local_uo(i, o):
     return u_o
 
 
-#calculating local perceived utility
-# def local_ul(i, g):
-#     u_l = 0
-#     for j in g.neighbors(i):
-#         u_l += g.edge[i][j]['w'] * g.node[i]['s'] * g.node[j]['s']
-#     return u_l
-
-
 def global_uo(o):
     U = 0
     for i in o.nodes():
         U += local_uo( i, g )
     return U
 
-#Plotting global perceived utility
-# def global_ul(g):
-#     U_l = 0
-#     for i in g.nodes():
-#         U_l += local_ul( i, g )
-
-def node_state(i, g):
+def node_state():
+    i = rd.choice(g.nodes())
+    
     m_1 = 0
-    for j in g.neighbors(i):
-        m_1 += g.edge[i][j]['w'] * -1 * g.node[j]['s']
+    for j in o.neighbors(i):
+        m_1 += (o.edge[i][j]['w'] + g.edge[i][j]['w']) * -1 * o.node[j]['s']
    
 
     m_2 = 0
-    for j in g.neighbors(i):
-        m_2 += g.edge[i][j]['w'] * 1 * g.node[j]['s']
+    for j in o.neighbors(i):
+        m_2 += (o.edge[i][j]['w'] + g.edge[i][j]['w']) * 1 * o.node[j]['s']
 
 
     if m_1 != m_2:
         if m_1 > m_2:
-            g.node[i]['s'] = -1
             o.node[i]['s'] = -1
         else:
-            g.node[i]['s'] = 1
             o.node[i]['s'] = 1
 
-
 def step():
-    global time, g, o, positions, pert_accu, perturbation_period, T
-    time += 1
-
-    if pert_accu == perturbation_period:
-        pert_accu = 0
-        randomize_states(g)
-    else:
-        pert_accu += 1
+    global time, g, o, positions, T
     
-
-    i = rd.choice(g.nodes())
-
-    r = 0.005
-    
-    m_1 = 0
-    for j in g.neighbors(i) and o.neighbors(i):
-        m_1 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] + r) * g.node[i]['s'] * g.node[j]['s']
-
-
-
-    m_2 = 0
-    for j in g.neighbors(i) and o.neighbors(i):
-        m_2 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] -r) * g.node[i]['s']  * g.node[j]['s']
-
-        
-    if m_1 != m_2:
-        if m_1 > m_2:
-            g.edge[i][j]['w'] += r
-        else:
-            g.edge[i][j]['w'] -= r
-
-    
-    node_state( i, g )
+    time = 0
+    randomize_states(o)
+    #steps = 1000
+    while time < 1000:
+        node_state()
+        time += 1
 
     if time%1000 == 0:
         T += 1
-        T_list.append( T )
-        U_plot.append( global_uo(o) )
-
+    if T > 100 and T < 300:
+        learning()
+    else:
+        node_state()
+        
+    T_list.append( T )
+    U_plot.append( global_uo(o) )
 
     time_list.append(time)
-    energy_state_o.append( global_uo(o) )
+    #energy_state_o.append( global_uo(o) )
     #energy_state_g.append( global_ul(g) )
+    
+def learning():
+#def step():
+    global time, g, o, positions, pert_accu, perturbation_period
+    #time += 1
+
+    # if pert_accu == perturbation_period:
+    #     pert_accu = 0
+    #     randomize_states(g)
+    # else:
+    #     pert_accu += 1
+    
+
+    #i = rd.choice(g.nodes())
+
+    r = 0.005
+    
+    # m_1 = 0
+    # for j in g.neighbors(i) and o.neighbors(i):
+    #     m_1 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] + r) * g.node[i]['s'] * g.node[j]['s']
+
+    for i in o.nodes():
+        m_1 = 0
+        for j in o.neighbors(i):
+            m_1 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] + r) * o.node[i]['s'] * o.node[j]['s']
+    
+
+        m_2 = 0
+        for j in o.neighbors(i):
+            m_2 += (g.edge[i][j]['w'] + o.edge[i][j]['w'] -r) * o.node[i]['s']  * o.node[j]['s']
+
+        
+        if m_1 != m_2:
+            if m_1 > m_2:
+                g.edge[i][j]['w'] += r
+            else:
+                g.edge[i][j]['w'] -= r
+
+
+    
     
 def no_draw():
     global time
