7b51899acec5e9b5546ec6bbe9085ec801321106
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-02-17 18:56:43 -0600

    refactor step function, omit pyxsymulator

--- simulations/2_testing_hebb.py
+++ simulations/2_testing_hebb.py
@@ -14,7 +14,7 @@ time_list = []
 energy_state_g = []
 energy_state_o = []
 
-perturbation_period = 1001
+perturbation_period = 1000
 pert_accu = 0
 time = 0
 T = 0
@@ -23,7 +23,7 @@ T_list = [0, ]
 U_plot = [0, ]
 
 def init_full():
-    global time, g, o, positions
+    global g, o
 
     g = nx.complete_graph(50) 
 
@@ -40,14 +40,16 @@ def init_full():
         o.edge[i][j]['w'] = rd.choice([1,0,-1])
 
 def init_erdos():
-    global time, g, o, positions, U
+    global g, o
     
-    time = 0
     g = nx.erdos_renyi_graph(50, .07)
 
     for i in g.nodes():
         g.node[i]['s'] = rd.choice([1,-1])
 
+    for i,j in g.edges():
+        g.edge[i][j]['w'] = 0
+        
     o = g.copy()
     for i,j in o.edges():
         o.edge[i][j]['w'] = rd.choice([-1,0,1])
@@ -83,7 +85,7 @@ def global_uo(o):
     return U
 
 def node_state(i,o):
-    #i = rd.choice(g.nodes())
+    global g
     
     m_1 = 0
     for j in o.neighbors(i):
@@ -102,56 +104,36 @@ def node_state(i,o):
             o.node[i]['s'] = 1
 
 def step():
-    global time, o, positions, T, perturbation_period, pert_accu
+    global time, o, g, T, perturbation_period, pert_accu
     
     time +=1
     
-    # if time == 1001:
-    #     randomize_states(o)
-
     if pert_accu == perturbation_period:
         pert_accu = 0
+        T += 1
+        T_list.append( T )
+        U_plot.append( global_uo(o) )
         randomize_states(o)
     else:
         pert_accu += 1
 
     i = rd.choice(g.nodes())
-    node_state(i,o)
-
-    if time%1000 == 0:
-         T += 1
-         T_list.append( T )
-         U_plot.append( global_uo(o) )
 
     if T > 100 and T < 200:
         learning()
-    else:
-        node_state(i,o)
 
-#    T_list.append( T )
-#    U_plot.append( global_uo(o) )
+    node_state(i,o)
 
     time_list.append(time)
     energy_state_o.append( global_uo(o) )
     #energy_state_g.append( global_ul(g) )
+
     
 def learning():
-#def step():
-    global  g, o, positions, pert_accu, perturbation_period
-    #time += 1
-
-    if pert_accu == perturbation_period:
-        pert_accu = 0
-        randomize_states(o)
-    else:
-        pert_accu += 1
-    
-
-    #i = rd.choice(g.nodes())
+    global  g, o
 
     r = 0.005
     
-
     for i in o.nodes():
         m_1 = 0
         for j in o.neighbors(i):
@@ -170,26 +152,34 @@ def learning():
                 g.edge[i][j]['w'] -= r
 
 
-    node_state(i, o)
+
     
 def no_draw():
     global time
     print time
 
         
-import pycxsimulator
+#import pycxsimulator
 #init()
-init_full()
+
 #init_watts()
 #init_erdos()
 #init_barabasi()
-positions = nx.spring_layout(g)
-pycxsimulator.GUI().start(func = [init_full, no_draw, step])
+#positions = nx.spring_layout(g)
+#pycxsimulator.GUI().start(func = [init_full, no_draw, step])
 #pycxsimulator.GUI().start(func = [init_full, draw, step])
+
+init_erdos()
+for n in xrange(300000):
+    # no_draw()
+    step()
+
+
+
 plt.cla()
 #plt.plot(time_list, energy_state_g, 'b+')
 #plt.plot(time_list, energy_state_o, 'r-')
 plt.scatter( T_list, U_plot, c=u'r', marker=u'D' )
 plt.xlabel('Time')
 plt.ylabel('Global Utility')
-plt.savefig('learning_plot.png')
+plt.savefig('learning_plot_erdos.png')
