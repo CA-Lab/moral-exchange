7829017c029543b2a211d3597f3206179a58150d
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-03-23 13:09:41 -0600

    set states, fitness and trust with a function

--- simulations/gt_trust_network/initializators.py
+++ simulations/gt_trust_network/initializators.py
@@ -10,63 +10,41 @@ def init_simple():
     g = nx.Graph()
     g.add_nodes_from(['a','b','c'])
 
+    # complete graph
     for i in g.node:
         for j in g.node:
-            # trust
             g.add_edge(i,j,w=10)
-
-    for i in g.nodes():
-        # choose random state
-        g.node[i]['s'] = rd.choice([C,D])
-        # all start with same fitness
-        g.node[i]['f'] = 10
-        
+            
+    g = set_nodes_and_edges(g)        
     return g
 
 
 def init_watts():
     g = nx.watts_strogatz_graph(100, 2, 0.3)
-
-    for i in g.nodes():
-        # choose random state
-        g.node[i]['s'] = rd.choice([C,D])
-        # all start with same fitness
-        g.node[i]['f'] = 10
-
-    for e in g.edges():
-        g.add_edge(*e, w=10)
-
+    g = set_nodes_and_edges(g)
     return g
         
 
 def init_erdos():
-    E = 0
-    F = 0
-    time = 0
     g = nx.erdos_renyi_graph(100, .3)
-
-    for i in g.nodes():
-        # choose random state
-        g.node[i]['s'] = rd.choice([C,D])
-        # all start with same fitness
-        g.node[i]['f'] = 10
-
-    for e in g.edges():
-        g.add_edge(*e, w=10)
-
+    g = set_nodes_and_edges(g)
     return g
         
 def init_barabasi():
-
     g = nx.barabasi_albert_graph(200, 15)
+    g = set_nodes_and_edges(g)
+    return g
 
+
+
+def set_nodes_and_edges(g, fitness=10, trust=10):
     for i in g.nodes():
         # choose random state
         g.node[i]['s'] = rd.choice([C,D])
         # all start with same fitness
-        g.node[i]['f'] = 10
+        g.node[i]['f'] = fitness
 
     for e in g.edges():
-        g.add_edge(*e, w=10)
+        g.add_edge(*e, w=trust)
 
     return g
