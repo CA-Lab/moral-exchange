93657f58a1b9e261135927fc1b95e9f88563d6fb
jmsiqueiros <jmsiqueiros@member.fsf.org>
2015-04-22 12:50:09 -0500

    Added code for running the simulation under a scale-free directed network.

--- simulations/gt_trust_network/in_memory_simulation.py
+++ simulations/gt_trust_network/in_memory_simulation.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(description='Prissoner\'s dilema with trust net
 parser.add_argument('--runid', default="" )
 parser.add_argument('--iterations', type=int, default=50 )
 parser.add_argument('--optimize', default="probabilistic", choices=['fitness', 'trust', 'balance', 'majority', 'probabilistic'] )
-parser.add_argument('--init', default="erdos", choices=['erdos', 'full', 'di_erdos', 'di_watts', 'watts', 'barabasi'] )
+parser.add_argument('--init', default="erdos", choices=['erdos', 'full', 'di_erdos', 'di_watts', 'watts', 'barabasi', 'di_scale_free'] )
 parser.add_argument('--step', default="sync", choices=['async', 'sync'] )
 
 args = parser.parse_args()
@@ -444,7 +444,8 @@ elif args.init == 'di_watts':
     g = init_directed_watts()
 elif args.init == 'barabasi':
     g = init_barabasi()
-
+elif args.init == 'di_scale_free':
+    g = init_di_scale_free()
 
 
 
