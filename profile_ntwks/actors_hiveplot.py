3abfed1affe20614760ef3688699a76469ad6e45
jmsiqueiros <jmsiqueiros.bioetica@gmail.com>
2015-06-06 19:39:37 -0500

    It plots node size according to node's degree

--- profile_ntwks/actors_hiveplot.py
+++ profile_ntwks/actors_hiveplot.py
@@ -3,7 +3,7 @@ import networkx as nx
 import random
 
 
-h = Hiveplot( 'cerrito_actores.svg')
+h = Hiveplot( 'cerrito_degree.svg')
 
 
 axis0 = Axis( (200,350), # start
@@ -32,49 +32,57 @@ the_axes = [ axis0,
              axis2, ]
 
 
-civil = ['Col_Vereda_Cerrito', 'Docentes','UniPamplona', 'Universidades', 'MAFRACOL', 'APPA', 'ARTELANA', 'ARTEPAJA', 'ASOCAPRISER', 'ASOGANACER', 'ASOMATE', 'ASOMOARCE', 'ASOPAGAR', 'ASOPIGAR', 'Campesinos', 'CDS', 'CEI', 'Comunidad', 'Cristina_Rep_Legal_ASOMOARCE', 'DC,' 'Estudiantes', 'Finquero', 'Gloria_Calderon', 'Gps_Armados', 'Gps_Conservacion', 'Gps_Ecologistas', 'Jose_Florez_Historico', 'Lideres_Comunales', 'Maria_Florez_Historico', 'Normal_Pie_de_Cuesta', 'Normal', 'ONGs', 'Organizaciones', 'Padre_historico_1954', 'Padres_de_Familia', 'Serafito_Calderon', 'UAK/UWA', 'UIS', 'William_Bastos']
+civil = ['Col_Vereda_Cerrito', 'Docentes','UniPamplona', 'Universidades', 'MAFRACOL', 'APPA', 'ARTELANA', 'ARTEPAJA', 'ASOCAPRISER', 'ASOGANACER', 'ASOMATE', 'ASOMOARCE', 'ASOPAGAR', 'ASOPIGAR', 'Campesinos', 'CDS', 'CEI', 'Comunidad', 'DC', 'Estudiantes', 'Finquero', 'Gloria_Calderon', 'Gps_Armados', 'Gps_Conservacion', 'Gps_Ecologistas', 'Jose_Florez_(Historico)', 'Lideres_Comunales', 'Maria_Florez_(Historico)', 'Normal_Pie_de_Cuesta', 'Normal', 'ONGs', 'Organizaciones', 'Padre_(historico_1954)', 'Padres_de_Familia', 'Serafito_Calderon', 'UAK/UWA', 'UIS', 'William_Bastos']
 
-gobierno = ['Ceres', 'CGR', 'Hospital', 'ICA', 'SENA', 'Admon_Mpal', 'Alcalde', 'Alcaldia', 'ASOJUNTAS', 'BanAgrario', 'Bomberos', 'Camacho_Historico', 'CAS', 'COANDIS_Asoc_Mpal', 'Comisaria_Familia', 'COMULDESPA', 'Concejo_Mpal', 'Gob_Arauca', 'Gob_Boyaca', 'Gob_Casanare', 'Gob_Cundinamarca', 'Gobernacion', 'Gobierno', 'Gob_Santander', 'Ing_Planeacion', 'Inspeccion_de_policia', 'MinAgricultura', 'MinAmbiente', 'Of_Saneamiento_Ambiental', 'Personeria', 'Policia', 'Presidencia', 'Prov_Garcia_Rovira', 'Sec_Gobierno', 'Uribe']
+gobierno = ['Ceres', 'CGR', 'Hospital', 'ICA', 'SENA', 'Admon_Mpal', 'Alcalde', 'Alcaldia', 'ASOJUNTAS', 'BanAgrario', 'Bomberos', 'Camacho_(Historico)', 'CAS', 'COANDIS_(Asoc_Mpal)', 'Comisaria_Familia', 'COMULDESPA', 'Concejo_Mpal', 'Gob_Arauca', 'Gob_Boyaca', 'Gob_Casanare', 'Gob_Cundinamarca', 'Gobernacion', 'Gobierno', 'Gob_Santander', 'Ing_Planeacion', 'Inspeccion_de_policia', 'MinAgricultura', 'MinAmbiente', 'Of_Saneamiento_Ambiental', 'Personeria', 'Policia', 'Presidencia', 'Prov_Garcia_Rovira', 'Sec_Gobierno', 'Uribe']
 
 empresas = ['CARBORIENTE', 'Empresas', 'Empresas-RQ', 'FRESCALECHE', 'Mineras', 'Multinacionales', 'Queserias']
 
 
+#for node in g.nodes():
+
 offset = 0.0255
 for n in gobierno:
+    #print nx.degree(g, n), n
     nd = Node(n)
     axis0.add_node(nd, offset)
     offset += 0.0255
     nd.dwg = nd.dwg.circle(center = (nd.x, nd.y),
-                           r      = 3,
-                           fill   = 'yellow',
-                           fill_opacity = 0.5,
-                           stroke = 'black',
-                           stroke_width = 0.5)
-
+                               #r      = 3,
+                               r      = nx.degree(g, n),
+                               fill   = 'yellow',
+                               fill_opacity = 0.5,
+                               stroke = 'black',
+                               stroke_width = 0.5)
+        
 offset = 0.024
 for n in civil:
+    #print nx.degree(g, n), n
     nd = Node(n)
     axis1.add_node(nd, offset)
     offset += 0.024
     nd.dwg = nd.dwg.circle(center = (nd.x, nd.y),
-                           r      = 3,
-                           fill   = 'blue',
-                           fill_opacity = 0.5,
-                           stroke = 'black',
-                           stroke_width = 0.5)
+                                #r      = 3,
+                                r      = nx.degree(g, n),
+                                fill   = 'blue',
+                                fill_opacity = 0.5,
+                                stroke = 'black',
+                                stroke_width = 0.5)
     
     
 offset = 0.1
 for n in empresas:
+    #print nx.degree(g, n), n
     nd = Node(n)
     axis2.add_node(nd, offset)
     offset += 0.1
     nd.dwg = nd.dwg.circle(center = (nd.x, nd.y),
-                           r      = 3,
-                           fill   = 'red',
-                           fill_opacity = 0.5,
-                           stroke = 'black',
-                           stroke_width = 0.5)
+                                #r      = 3,
+                                r      = nx.degree(g, n),
+                                fill   = 'red',
+                                fill_opacity = 0.5,
+                                stroke = 'black',
+                                stroke_width = 0.5)
 
 # for n in g.nodes():
 #     nd = Node(n)
